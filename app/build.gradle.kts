bad06e7aa1c7f4ab5ea32f629af7082af290ad7e
Davi_Moretti_PC <daviabelmoretti1@gmail.com>
2024-11-25 17:13:57 -0300

    commit inicial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.davimoretti.horadocafe"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.davimoretti.horadocafe"
